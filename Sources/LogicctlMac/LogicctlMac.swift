72805af98d1d57b95f8e19cb28d572d2be2a3fc4
juliantellez <4896851+juliantellez@users.noreply.github.com>
2026-09-24 22:09:56 +0000

    revert: take the loose event out of the mac module

--- Sources/LogicctlMac/LogicctlMac.swift
+++ Sources/LogicctlMac/LogicctlMac.swift
@@ -1,4 +1,3 @@
-import CoreGraphics
 import LogicctlCore
 
 /// The part of logicctl that talks to macOS: Accessibility, CoreMIDI and posted events.
@@ -10,9 +9,4 @@ public enum LogicctlMac {
 
   /// The name of the module this one is built on.
   public static let coreModuleName = LogicctlCore.moduleName
-
-  /// An event made outside the gate, which the scan of Sources must refuse.
-  public static func looseEvent() -> CGEvent? {
-    CGEvent(source: nil)
-  }
 }
