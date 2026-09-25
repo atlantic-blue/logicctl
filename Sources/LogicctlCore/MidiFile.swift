942ff022cb6f7335496ec7ea3b0cb54f6785b2d6
juliantellez <4896851+juliantellez@users.noreply.github.com>
2026-09-25 02:51:57 +0000

    test: mutate the writer to keep the notes in the order the file listed them

--- Sources/LogicctlCore/MidiFile.swift
+++ Sources/LogicctlCore/MidiFile.swift
@@ -84,7 +84,7 @@ public enum MidiFile {
         Event(
           tick: starts + held, isOff: true, pitch: note.pitch, channel: note.channel, velocity: 0))
     }
-    return out.sorted { comes($0, before: $1) }
+    return out
   }
 
   /// Which of two events is written first.
